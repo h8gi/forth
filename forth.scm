dad24a55069a2558b498124c26b10939c94591da
hiroki yagi <hiro.yagi0245@gmail.com>
2016-03-24 01:50:21 +0900

    add compile state

--- forth.scm
+++ forth.scm
@@ -3,6 +3,23 @@
 ;;; token 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; GLOBAL
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; state 
+(define current-state (make-parameter 'interpret))
+(define (compile?)
+  (eq? (current-state) 'compile))
+(define (interpret?)
+  (eq? (current-state) 'interpret))
+
+;;; for word definition
+(define compile-stack (make-parameter '()))
+(define (compile-stack-push! proc)
+  (compile-stack (cons proc (compile-stack))))
+(define (compile-stack-clear!)
+  (compile-stack '()))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; LEXER?
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define (space-char? char)
@@ -60,12 +77,32 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; DICTIONARY 辞書の検索とか
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define (make-entry name proc)
-  (cons name proc))
+
+;;; 実行時のprocとcompile時のprocを指定
+(define (make-entry name proc #!key
+                    (immediate #f)
+                    (compile-only #f))
+  (list name proc immediate compile-only))
+
 (define (entry-name entry)
-  (car entry))
+  (first entry))
 (define (entry-body entry)
-  (cdr entry))
+  (second entry))
+(define (entry-immediate? entry)
+  (third entry))
+(define (entry-compile-only? entry)
+  (fourth entry))
+(define (entry-execute entry)
+  ((entry-body entry)))
+(define-syntax make-dictionary
+  (syntax-rules ()
+    [(_ (name body ...) ...)
+     (list (make-entry name body ...) ...)]))
+(define-syntax dictionary-push!
+  (syntax-rules ()
+    [(_ entry dict)
+     (set! dict (cons entry dict))]))
+
 
 ;;; 2項演算子用 -や/の順序に対応
 (define (binary-op op)
@@ -96,10 +133,36 @@
            'done]
           [else (skip-online-comment)])))
 
-(define-syntax make-dictionary
-  (syntax-rules ()
-    [(_ (name body) ...)
-     (list (make-entry name body) ...)]))
+;;; compile word ------------------------------
+;;; : colon
+(define (colon-define)
+  (let ([name (next-token)])
+    (cond [name
+           (compile-stack-clear!)
+           (compile-stack-push! name)   ; word name
+           (current-state 'compile)     ; start compile
+           ]
+          [else 'done])))
+;;; convert compile stack to entry
+;;;  (proc ... name) -> (make-entry name new-proc)
+(define (compile-stack->entry)
+  (let* ([stk (reverse! (compile-stack))]
+         [name (car stk)]
+         [proc-list (cdr stk)]
+         [body (lambda ()
+                 (for-each (lambda (proc)
+                             (proc))
+                           proc-list))]
+         [entry (make-entry name body)])
+    entry))
+;;; ; end compile
+(define (semicolon)
+  (dictionary-push! (compile-stack->entry) global-dictonary)
+  (current-state 'interpret)
+  'done)
+
+;;; control structure ------------------------------
+;;; if
 
 (define global-dictonary  
   (make-dictionary
@@ -119,33 +182,72 @@
    ("-" (binary-op -))
    ("/" (binary-op /))
    ;; comment
-   ("(" skip-comment)
-   (".(" display-comment)
-   ("\\" skip-online-comment)
+   ("(" skip-comment
+    #:immediate #t)
+   (".(" display-comment
+    #:immediate #t)
+   ("\\" skip-online-comment
+    #:immediate #t)
+   ;; compile word
+   ;("create" )
+   (":" colon-define)
+   (";" semicolon
+    #:immediate #t)
+   ;; if compile時の意味を考えればよい
+   ;; endifまでを一纏めにする
+   ;; ("if" (lambda ()
+   ;;         (let loop ([tkn (next-token)]
+   ;;                    [true-clause '()]
+   ;;                    [false-clause '()])
+   ;;           (when tkn
+   ;;             (cond [(string-ci=? tkn "else")]))))
+   ;;  #:immediate #t
+   ;;  #:compile-only #t)
    ))
 
 (define (search-dictionary dict word-name)
   (assoc word-name dict string-ci=?))
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; INTERPRETER
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (forth-abort #!optional (msg ""))
+(define (forth-abort . msgs)
   (d-clear!)
   (r-clear!)
-  (fprintf (current-error-port) "FORTH ERROR: ~A\n" msg))
+  (display  "FORTH ERROR:" (current-error-port))
+  (for-each (cut fprintf (current-error-port) " ~A" <>)
+            msgs)
+  (newline (current-error-port)))
 
-(define (forth-eval-token token dict)
+(define (forth-interpret token dict)
   (cond [(search-dictionary dict token)
-         => (lambda (entry) ((entry-body entry)))]
+         => (lambda (entry) (entry-execute entry))]
         [(string->number token)
          => (lambda (num)
               (d-push! num))]
         [else (d-clear!)
-              (forth-abort token)
-              (newline)]))
+              (forth-abort token)]))
+
+(define (forth-compile token dict)
+  (cond [(search-dictionary dict token)
+         => (lambda (entry)
+              (if (entry-immediate? entry)
+                  (entry-execute entry)
+                  (compile-stack-push! (entry-body entry))))]
+        [(string->number token)
+         => (lambda (num)
+              (compile-stack-push! (lambda ()
+                                  (d-push! num))))]
+        [else (d-clear!)
+              (forth-abort "compile state error" token)
+              (current-state 'interpret)]))
+
+
+(define (forth-eval-token token dict)
+  (if (compile?)
+      (forth-compile token dict)
+      (forth-interpret token dict)))
 
 (define (forth-loop)
   (let loop ([tkn (next-token)])
--- test.fs
+++ test.fs
@@ -3,3 +3,14 @@
 * .s
 clear .s
 
+( hello hello) .s
+.( hello hello)
+\ hello 
+1 2 3 .s
+clear
+.s
+: hello .( this is comment )
+    10 20 30 ;
+hello 
+.s
+clear
