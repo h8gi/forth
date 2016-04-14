2ae3d44e4f827fc4bacd5936a8526b3d9536baaf
yagi <hiro.yagi0245@gmail.com>
2016-04-14 23:35:06 +0900

    remove useless import

--- test.fs
+++ test.fs
@@ -2,7 +2,7 @@
 : hoge POSTPONE push-10 ; immediate
 : a hoge ; ( a push-10 )
 
-: elsif
+: elsif-test
     dup 10 <
     if  ." smaller than 10"
     else
@@ -12,6 +12,15 @@
 	    dup 30 <
 	    if ." smaller than 30"
 	    then
-	then
-    then drop ." finish ";
+	then    
+    then ." finish" ;
+
+0  elsif-test
+
+10 elsif-test
+
+20 elsif-test
+
+30 elsif-test
+
 
